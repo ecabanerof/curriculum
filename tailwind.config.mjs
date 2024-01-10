f7fa9f7512a422e8b37a6a6f7f22669d9834114a
Emilio Cabañero <e.cabanero@hotmail.com>
2024-01-10 18:11:00 +0100

    First version

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,6 +12,7 @@ export default {
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
 		extend: {},
 	},
